b3b5e32fc18731712d50bc78f2d5f64e5cb63426
rmulshine <rmulshi2@nd.edu>
2022-03-28 18:57:13 -0400

    Add the documentations for dependent example dataframes.

---
+++ R/dep_ex_df1.R
@@ -0,0 +1,10 @@
+#' Testing Scores of 100 Subjects from Rater 1 in Dependent Case
+#'
+#' A dataset containing scores with 4 trials for 100 subjects taken by
+#' the first rater. This is to be used in an example for the case of
+#' comparing 2 dependent ICCs (function dep_ci()), where this dataset
+#' contains the scores measured by Rater 1 of the 100 subjects and dataset
+#' dep_ex_df2 contains the scores measured by Rater 2 of the same 100 subjects.
+#'
+#' @format A dataframe with 100 rows and 4 columns
+"dep_ex_df1"

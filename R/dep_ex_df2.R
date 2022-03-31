66e85a2cb87a66fac560e224dd30089e5a2c38e8
rmulshine <rmulshi2@nd.edu>
2022-03-31 10:58:07 -0400

    Spelling changes to documentation

--- R/dep_ex_df2.R
+++ R/dep_ex_df2.R
@@ -1,10 +1,10 @@
 #' Testing Scores of 100 Subjects from Rater 2 in Dependent Case
 #'
 #' A dataset containing scores with 4 trials for 100 subjects taken by
-#' the second rater. This is to be used in an example for the case of
+#' the second rater/time. This is to be used in an example for the case of
 #' comparing 2 dependent ICCs (function dep_ci()), where this dataset
-#' contains the scores measured by Rater 2 of the 100 subjects and dataset
-#' dep_ex_df1 contains the scores measured by Rater 1 of the same 100 subjects.
+#' contains the scores measured by Rater/Time 2 of the 100 subjects and dataset
+#' dep_ex_df1 contains the scores measured by Rater/Time 1 of the same 100 subjects.
 #'
 #' @format A dataframe with 100 rows and 4 columns
 "dep_ex_df2"

c008c9e96a4d3e7381fcd80da9510fa6d9ad57c0
rmulshine <rmulshi2@nd.edu>
2022-03-31 11:26:56 -0400

    Added independent example dataframes and respective documentations

---
+++ R/indep_ex_df1.R
@@ -0,0 +1,11 @@
+#' Testing Scores of 100 Subjects from Rater/Time 1 in Independent Case
+#'
+#' A dataset containing scores with 4 trials for 100 subjects taken by
+#' the first rater/time. This is to be used in an example for the case of
+#' comparing 2 independent ICCs (function indep_ci()), where this dataset
+#' contains the scores measured by Rater/Time 1 of 100 subjects and dataset
+#' indep_ex_df2 contains the scores measured by Rater/Time 2 of a different
+#' set of 80 subjects.
+#'
+#' @format A dataframe with 100 rows and 4 columns
+"indep_ex_df1"
---
+++ R/indep_ex_df2.R
@@ -0,0 +1,11 @@
+#' Testing Scores of 80 Subjects from Rater/Time 2 in Independent Case
+#'
+#' A dataset containing scores with 4 trials for 80 subjects taken by
+#' the second rater/time. This is to be used in an example for the case of
+#' comparing 2 independent ICCs (function indep_ci()), where this dataset
+#' contains the scores measured by Rater/Time 2 of 80 subjects and dataset
+#' indep_ex_df1 contains the scores measured by Rater/Time 1 of a different
+#' set of 100 subjects.
+#'
+#' @format A dataframe with 80 rows and 4 columns
+"indep_ex_df1"
